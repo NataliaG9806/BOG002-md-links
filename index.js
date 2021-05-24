34d1f00f43dc300ab6f40479352673b2bac56b5b
NataliaG9806 <garavitobarretonatalia@gmail.com>
2021-05-24 08:48:44 -0500

    nuevo: Leer contenido y obtener extension de un archivo con una ruta fija

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+//   // ...
+// };
+
+
+let fs = require('fs');
+let path = require('path') 
+
+const route = 'fileToRead.md';
+
+
+
+fs.readFile(route, {encoding: 'utf-8'}, function (error, datos) {
+  if (error){
+    console.log('Error : ${error}');
+  } else{
+    console.log('Datos en el archivo: ');
+    console.log(datos);
+    const routeExtension = path.extname(route)
+    console.log('la extensión del archivo es: ' + routeExtension);
+
+  }
+})
